828d71af0ff091dd52150933b3de7e9e86d81977
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-03-30 13:00:29 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -13,20 +13,7 @@ const connectDB = async () => {
     });
     
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    
-    // Handle connection events
-    mongoose.connection.on('error', (err) => {
-      console.error('MongoDB connection error:', err);
-    });
-    
-    mongoose.connection.on('disconnected', () => {
-      console.log('MongoDB disconnected');
-    });
-    
-    mongoose.connection.on('reconnected', () => {
-      console.log('MongoDB reconnected');
-    });
-    
+    return conn;
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
